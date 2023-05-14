e072705e6a6ec0ffe9d18598ba19a2f51f8cd794
fastcoderultimate <133053898+fastcoderultimate@users.noreply.github.com>
2023-05-14 12:25:56 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,31 @@
 const stairsContainer = document.querySelector(".stairs_container");
 
-const size = Number(stairsContainer.getAttribute("size")); // Convert to Number //
+const size = Number(stairsContainer.getAttribute("size"));
 const itemQty = Number(stairsContainer.getAttribute("itemQty"));
 
-const stairsItemSize = size / itemQty;
+const itemSpacious = size / (itemQty + 1);
 
 stairsContainer.setAttribute(
   "style",
-  `--stairs_size: ${size}px; --stair_item_size: ${stairsItemSize}px`
+  `--size: ${size}px; --item_spacious: ${itemSpacious}px`
 );
 
 const stairs = document.createElement("div");
 stairs.classList.add("stairs");
 
-stairsContainer.appendChild(stairs);
-
-// Create stairs item
-for (var i = 1; i <= itemQty; i++) {
+// Create items
+for (var i = 0; i < itemQty; i++) {
   const item = document.createElement("div");
   item.classList.add("item");
+
   item.setAttribute("style", `--i: ${i}`);
 
-  const childDiv = document.createElement("div");
-  item.appendChild(childDiv);
+  const div1 = document.createElement("div");
+  const div2 = document.createElement("div");
+
+  item.appendChild(div1);
+  item.appendChild(div2);
 
   stairs.appendChild(item);
 }
+stairsContainer.appendChild(stairs);
